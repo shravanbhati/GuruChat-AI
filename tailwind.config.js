cb4c47003737b495d31838c20704bf3c2413514f
Shravan Bhati <dev.shravanbhati@gmail.com>
2025-08-15 10:48:02 +0000

    Background grid fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}", // adjust to match your file paths
+    "./app/**/*.{js,ts,jsx,tsx}", // adjust to match your file paths
   ],
   plugins: [require("@tailwindcss/typography")],
 };
