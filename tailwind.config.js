c3eabf461136ec85a86f217fd2f2888747ae3214
Shravan Bhati <dev.shravanbhati@gmail.com>
2025-08-15 11:38:38 +0000

    try&catch

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // adjust to match your file paths
-  ],
-  plugins: [require("@tailwindcss/typography")],
-};
